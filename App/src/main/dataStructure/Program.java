2cfd235f998d83547644f6ac22cdf42b7f4f7e24
edward-qin <3dwardqin@gmail.com>
2022-03-23 02:41:10 -0700

    setup

---
+++ App/src/main/dataStructure/Program.java
@@ -0,0 +1,22 @@
+
+public class Program{
+    public static void main(String[] args){
+
+        /*
+        array
+        linkedList
+        stack
+        queue
+
+        binary tree
+        binary search tree -
+        binary heap -
+        hashing -
+
+        graph
+        trie -
+        segment tree -
+        suffix tree -
+         */
+    }
+}
