05fa5b26a930a52480a9de70fdc6bf49458cb755
edward-qin <3dwardqin@gmail.com>
2022-06-21 00:38:26 -0700

    gradlew

--- App/src/main/dataStructure/Program.java
+++ App/src/main/dataStructure/Program.java
@@ -3,20 +3,7 @@ public class Program{
     public static void main(String[] args){
 
         /*
-        array
-        linkedList
-        stack
-        queue
-
-        binary tree
-        binary search tree -
-        binary heap -
-        hashing -
-
-        graph
-        trie -
-        segment tree -
-        suffix tree -
+       a couple ideas
          */
     }
 }
